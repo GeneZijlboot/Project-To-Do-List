e880870c8ab66568af543a057f8e9697f0944761
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-07-19 16:14:12 +0200

    almost there

--- src/AddToDo.js
+++ src/AddToDo.js
@@ -1,3 +1,68 @@
+const SubmitButton = document.createElement('input');
+const Form = document.createElement('form');
+const Screen = document.getElementById('Screen');
+const ToDoNotes = document.createElement("div");
+ToDoNotes.id = ToDoNotes;
+
+SubmitButton.type = "submit";
+SubmitButton.style = "cursor: pointer;";
+SubmitButton.id = "SubmitButton";
+Form.id = "AddTaskform";
+
+const NewTask = document.createElement('div');
+const txt = document.createElement('h2');
+const Title = document.createElement('input');
+const brONE = document.createElement('br');
+const Description = document.createElement('input');
+const brTWO = document.createElement('br');
+const div = document.createElement('div');
+const DueDate1 = document.createElement('label');
+const DueDate2 = document.createElement('input');
+const brThree = document.createElement('br');
+const div2 = document.createElement('div');
+const Priority = document.createElement('label');
+const Priority2 = document.createElement('select');
+const Low = document.createElement('option');
+const Medium = document.createElement('option');
+const High = document.createElement('option');
+const brFOUR = document.createElement('br');
+
+NewTask.id = "NewTask";
+txt.class = "text";
+DueDate1.for = "DueDate";
+DueDate2.id = "DueDate";
+Priority.for = "Priority";
+Priority2.id = "Priority";
+
+div.style = "display:flex;"
+
+Title.id = "Title";
+Title.type = "text";
+Title.name = "Title";
+Title.placeholder = "Title";
+
+DueDate2.type = "date";
+DueDate2.name = "DueDate";
+Priority2.name = "Priority";
+
+Description.id = "Description";
+Description.type = "text";
+Description.name = "Description";
+Description.placeholder = "Description";
+
+txt.textContent = "New Task";
+DueDate1.textContent = "Due Date: ";
+
+Priority.textContent = "Priority: ";
+
+Low.value = "Low";
+Medium.value = "Medium";
+High.value = "High";
+
+Low.textContent = "Low";
+Medium.textContent = "Medium";
+High.textContent = "High";
+
 //function for making the text and add a "ToDo" button
 export function AddToDo(){
     const TaskBar = document.getElementById('TaskBar');
@@ -15,67 +80,6 @@ export function AddToDo(){
 export function ToDoForm(){
     const AddToDo = document.getElementById('AddTaskButton');
     AddToDo.addEventListener('click', () => {
-        const Screen = document.getElementById('Screen');
-        const NewTask = document.createElement('div');
-        const Form = document.createElement('form');
-        const txt = document.createElement('h2');
-        const Title = document.createElement('input');
-        const brONE = document.createElement('br');
-        const Description = document.createElement('input');
-        const brTWO = document.createElement('br');
-        const div = document.createElement('div');
-        const DueDate1 = document.createElement('label');
-        const DueDate2 = document.createElement('input');
-        const brThree = document.createElement('br');
-        const div2 = document.createElement('div');
-        const Priority = document.createElement('label');
-        const Priority2 = document.createElement('select');
-        const Low = document.createElement('option');
-        const Medium = document.createElement('option');
-        const High = document.createElement('option');
-        const brFOUR = document.createElement('br');
-        const SubmitButton = document.createElement('input');
-
-        NewTask.id = "NewTask";
-        Form.id = "AddTaskform";
-        txt.class = "text";
-        DueDate1.for = "DueDate";
-        DueDate2.id = "DueDate";
-        Priority.for = "Priority";
-        Priority2.id = "Priority";
-        SubmitButton.id = "SubmitButton";
-        SubmitButton.type = "submit";
-        SubmitButton.style = "cursor: pointer;";
-
-        div.style = "display:flex;"
-
-        Title.id = "Title";
-        Title.type = "text";
-        Title.name = "Title";
-        Title.placeholder = "Title";
-
-        DueDate2.type = "date";
-        DueDate2.name = "DueDate";
-        Priority2.name = "Priority";
-
-        Description.id = "Description";
-        Description.type = "text";
-        Description.name = "Description";
-        Description.placeholder = "Description";
-
-        txt.textContent = "New Task";
-        DueDate1.textContent = "Due Date: ";
-
-        Priority.textContent = "Priority: ";
-
-        Low.value = "Low";
-        Medium.value = "Medium";
-        High.value = "High";
-
-        Low.textContent = "Low";
-        Medium.textContent = "Medium";
-        High.textContent = "High";
-
         Screen.appendChild(NewTask);
         NewTask.appendChild(Form);
         Form.appendChild(txt);
@@ -97,12 +101,53 @@ export function ToDoForm(){
         Form.appendChild(SubmitButton);
 
         console.log('test');        
-
+        
         AddToDo.disabled = true;
 })
 }
 
 //function for every div of the "ToDo"
 export function ToDoDiv(){
-    console.log('functie werkt!')
+    let ToDo = [];
+
+    function ToDoItem(Title, Description, DueDate2, Priority2) {
+        this.Title = Title;
+        this.Description = Description;
+        this.DueDate2 = DueDate2;
+        this.Priority2 = Priority2;
+    }
+
+    function render(){
+        for(let i = 0; i < ToDo.length; i++){
+            let A = ToDo[i];
+            ToDoNotes.innerHTML = `
+                <div id="ToDo">
+                    <p>test</p>
+                    <p>${A.Title}<p>
+                    <p>${A.Description}<p>
+                    <p>${A.DueDate2}<p>
+                    <p>${A.Priority2}<p>
+                </div>
+            `;
+            Screen.appendChild(ToDoNotes);
+            Screen.removeChild(NewTask);
+        }
+    }
+
+    function AddToDo(){
+        const Title = document.getElementById('Title').value;
+        const Description = document.getElementById('Title').value;
+        const DueDate2 = document.getElementById('Title').value;
+        const Priority2 = document.getElementById('Title').value;
+
+        let newToDo = new ToDoDiv(Title, Description, DueDate2, Priority2)
+        ToDo.push(newToDo);
+        render();
+    }
+
+    SubmitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(Title.value, Description.value, DueDate2.value, Priority2.value);
+        AddToDo();
+    })
 }
